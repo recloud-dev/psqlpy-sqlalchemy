62f3642ee80c56326321c326f8b1f6011e8ad806
Daniil Babin <babindj@uriit.ru>
2026-07-07 11:50:40 +0500

    remove placeholder async dialect test and tidy suite whitespace

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,12 +1,13 @@
 # from sqlalchemy.testing.suite import *  # noqa: F401,F403
 from sqlalchemy.testing.suite import NumericTest as _NumericTest
 
+
 class NumericTest(_NumericTest):
     def test_decimal_coerce_round_trip(self, connection):
         super().test_decimal_coerce_round_trip(connection)
-        
+
     def test_render_literal_numeric(self, literal_round_trip):
         super().test_render_literal_numeric(literal_round_trip)
-        
+
     def test_float_as_decimal(self, do_numeric_test):
         super().test_float_as_decimal(do_numeric_test)
